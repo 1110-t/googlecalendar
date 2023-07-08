a62a22a4525866eab66e9085f9a96bd0e863e2d3
1110-t <contact@miraino-katachih2.co.jp>
2023-07-08 16:57:34 +0900

    end

--- js/calendar.js
+++ js/calendar.js
@@ -4,7 +4,7 @@ class Calendar {
     this.m = 0;
     this.week = ["日", "月", "火", "水", "木", "金", "土"];
     this.event = "";
-    this.time = {start:0,end:23};
+    this.time = {start:0,end:24};
   }
   create(...theArgs){
     let year = 0;
@@ -55,7 +55,11 @@ class Calendar {
           // 時間配列を要素として保持する
           // <p><span time='6'></span><span time='7'></span></p>
           for (var g = this.time.start; g <= this.time.end; g++) {
-            instance += "<span id='h" + g +"'><span minites='m0'></span><span minites='m15'></span><span minites='m30'></span><span minites='m45'></span></span>";
+            if(g != 24){
+              instance += "<span id='h" + g +"'><span minites='m0'></span><span minites='m15'></span><span minites='m30'></span><span minites='m45'></span></span>";
+            } else {
+              instance += "<span id='h" + g +"'><span minites='m0'></span></span>";
+            };
           };
           instance += "</p></td>";
         };
@@ -164,7 +168,7 @@ let c = new Calendar();
 c.display(nowYear,nowMonth);
 c.create(nowYear,nowMonth);
 
-let startHour = 0;
+startHour = "";
 class Day{
   constructor(day_element) {
     this.day_element = day_element;
@@ -174,19 +178,26 @@ class Day{
     this.startHour = 0;
     this.startMinites = 0;
   };
-  create(){
+  // カレンダー上の日付が選択された場合に、最初の時間選択のselectを生成する
+  createHourSelector(){
+    // すべての時間帯および分数のデータを取得する
     let all_time = this.day_element.querySelectorAll("span span");
     let select = document.createElement("select");
     all_time.forEach((item,i) => {
       let counter = 0;
+      // 15分刻みで最低利用時間の２時間分の間があるかチェックしていく
       for (var j = 0; j < this.time_length; j++) {
+        // ２時間後が、一日の終わりの時間を超えていなければ処理を行う
         if(all_time[i+j+1] != undefined){
+          // 予定があいており、次のステップの時間の予定もあいている場合
           if(item.textContent == "" && all_time[i+j+1].textContent == ""){
             counter += 1;
+            // ２時間後の指定になったとき
             if(counter == 8){
               counter = 0;
               let h = item.parentNode.getAttribute("id").replace("h","");
               let m = item.getAttribute("minites").replace("m","");
+              // まだオプションタグとして時間選択の要素がない場合
               if(!select.querySelector("[hours=h"+h+"]")){
                 let opt = document.createElement("option");
                 opt.setAttribute("value",h);
@@ -194,20 +205,22 @@ class Day{
                 opt.setAttribute("hours","h"+h);
                 opt.setAttribute("minites",m);
                 select.appendChild(opt);
+              // 時間選択の要素がある場合
               } else {
                 let opt = select.querySelector("[hours=h"+h+"]");
                 let minites = opt.getAttribute("minites");
                 minites = minites+" "+m;
                 opt.setAttribute("minites",minites);
               };
-              console.log(item.parentNode.getAttribute("id").replace("h","") + ":" + item.getAttribute("minites").replace("m",""));
             };
           };
         };
       };
+      // 用事が入っている場合
       if(item.textContent != ""){
         let h = item.parentNode.getAttribute("id").replace("h","");
         let m = item.getAttribute("minites").replace("m","");
+        // 用事の一番最初の時刻を登録する
         if(!select.querySelector("[hour=h"+h+"]")){
           let end_time = document.createElement("div");
           end_time.setAttribute("hour","h"+h);
@@ -216,17 +229,79 @@ class Day{
         };
       };
     });
+    // セレクタ―について出来上がったらフッターに追加する
     let calendar__footer = document.querySelector(".calendar__footer");
     calendar__footer.innerHTML = "";
     select.classList.add("hourSelector");
-    select.addEventListener("change",this.hourChange);
+    select.addEventListener("change",Day.prototype.createMiniteSelector);
     calendar__footer.appendChild(select);
   };
-  hourChange(){
-    console.log(this);
+  // 時間の次に分数を選択する画面を生成する
+  createMiniteSelector(){
     // 選択されたオプションの値を取得する
     let val = this.selectedOptions[0].value;
     startHour = val;
+    // 適応する分数を取得する
+    let minites = this.selectedOptions[0].getAttribute("minites");
+    minites = minites.split(" ");
+    let select = document.createElement("select");
+    select.classList.add("miniteSelector");
+    // 時間に紐づいて対応する分数を挿入する
+    minites.forEach((minite, i) => {
+      select.innerHTML += "<option value='"+minite+"'>"+minite+"</option>";
+    });
+    let calendar__footer = document.querySelector(".calendar__footer");
+    // 選択されたら、次に終わりの時間を選択できる要素を生成する
+    select.addEventListener("change",Day.prototype.createEndHourSelector);
+    calendar__footer.appendChild(select);
+  };
+  // 終わりの時間を選択できるように生成する
+  createEndHourSelector(){
+    let val = this.selectedOptions[0].value;
+    let startMinites = parseInt(val);
+    // 開始できる時刻を設定する
+    let minStart = parseInt(startHour) + 2;
+    let selector = document.querySelector(".hourSelector");
+    // 終了時刻を新しいセレクタ―として作成する
+    let select = document.createElement("select");
+    select.classList.add("endHourSelector");
+    // 最小時間の時間と分数で予定があるかどうかチェックする
+    for (var i = minStart; i < 24; i++) {
+      let temp = selector.querySelector("[hour=h"+i+"]");
+      // その時間の予定がなければ
+      if(!temp){
+        select.innerHTML += "<option value="+i+" minites='0 15 30 45'>"+i+"</option>"
+      // その時間の予定があった場合、何分まで大丈夫か
+      } else {
+        let tempMinites = parseInt(temp.getAttribute("minite").replace("m",""));
+        // [0,15,30,45]
+        let tempMinitesArray = [0,15,30,45];
+        if(startMinites < tempMinites){
+          let option = "<option value='"+i+"' minites='";
+          let index1 = tempMinitesArray.indexOf(startMinites);
+          let index2 = tempMinitesArray.indexOf(tempMinites);
+          tempMinitesArray = tempMinitesArray.slice(index1,index2);
+          tempMinitesArray.forEach((temp,j) => {
+            if(j != (tempMinitesArray.length - 1)){
+              option += (temp + " ");
+            } else {
+              option += (temp + "'");
+            };
+          });
+          option += (">"+ i +"</option>");
+          select.innerHTML += option;
+        };
+      }
+    };
+    console.log(select);
+    let calendar__footer = document.querySelector(".calendar__footer");
+    select.addEventListener("change",Day.prototype.createEndMiniteSelector);
+    calendar__footer.appendChild(select);
+  };
+  createEndMiniteSelector(){
+    // 選択されたオプションの値を取得する
+    let val = this.selectedOptions[0].value;
+    selectDay.ystartHour = val;
     // 適応する分数を取得する
     let minites = this.selectedOptions[0].getAttribute("minites");
     minites = minites.split(" ");
@@ -236,35 +311,18 @@ class Day{
       select.innerHTML += "<option value='"+minite+"'>"+minite+"</option>";
     });
     let calendar__footer = document.querySelector(".calendar__footer");
-    select.addEventListener("change",Day.prototype.miniteChange);
     calendar__footer.appendChild(select);
-  };
-  miniteChange(){
-    let val = this.selectedOptions[0].value;
-    let startMinites = val;
-    // 開始できる時刻を設定する
-    let minStart = startHour + 2;
-    let selector = document.querySelector(".hourSelector");
-    // 終了時刻を新しいセレクタ―として作成する
-    let select = document.createElement("select");
-    select.classList.add("endHourSelector");
-    // 最小時間の時間と分数で予定があるかどうかチェックする
-    for (var i = startHour; i < 24; i++) {
-      let temp = select.querySelector("[hour=h"+i+"]");
-      if(!temp){
-        select.innerHTML += "<option value="+i+"></option>"
-      };
-    };
-    console.log(start);
   };
 };
 
 
 let calendar_body = document.querySelector(".calendar__body");
+selectDay = "";
 calendar_body.addEventListener("click",function(e){
   let day_element = e.target.closest("td");
+  // その月の日付かチェック
   if(!day_element.classList.contains("disabled")){
-    let day = new Day(day_element);
-    day.create();
+    selectDay = new Day(day_element);
+    selectDay.createHourSelector();
   };
 });
